d1afd225c27d98d092b6062ee2bd09718e1fbc96
AntonioParra <antparu@gmail.com>
2023-07-05 19:24:46 +0200

    state timer

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -4,9 +4,10 @@ signal flee
 
 @export var flee_target: Player
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var timer: Timer = $Timer
 
 const WALK_SPEED = Constants.base_speed
-const FLEE_SPEED = WALK_SPEED * 1.25
+const FLEE_SPEED = WALK_SPEED * 1.4
 const FLEE_DISTANCE = 150
 const FLEE_DISTANCE_SQUARED = FLEE_DISTANCE * FLEE_DISTANCE
 
@@ -16,12 +17,10 @@ func _ready() -> void:
 	var sprite_index: int = randi() % 12
 	var texture = load("res://assets/img/characters/char" + str(sprite_index) + ".png")
 	sprite.texture = texture
-	
 	start_wandering()
-	
 
 func _physics_process(_delta):
-	if (flee_target != null) and (flee_target.position - position).length_squared() < FLEE_DISTANCE_SQUARED:
+	if timer != null && (flee_target != null) && (flee_target.position - position).length_squared() < FLEE_DISTANCE_SQUARED:
 		start_fleeing()
 		
 	if velocity.length_squared() > 0:
@@ -32,12 +31,20 @@ func get_speed() -> float:
 	return WALK_SPEED if not fleeing else FLEE_SPEED
 	
 func start_wandering() -> void:
+	timer.start()
 	velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 	fleeing = false
 	
 func start_fleeing() -> void:
+	timer.stop()
+	timer.start()
 	velocity = (position - flee_target.position).normalized()
 	if velocity.length() == 0:
 		velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 	fleeing = true
 	emit_signal("flee")
+
+
+func _on_timer_timeout() -> void:
+	velocity = Vector2.ZERO
+	start_wandering()
